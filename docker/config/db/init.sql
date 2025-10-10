7e29cf728f85f688f3fcbdd004769fc0f314f9ac
邢怀康 <calvin@cloverdeMacBook-Air.local>
2025-10-10 14:15:45 +0800

    docker-config update

--- docker/config/db/init.sql
+++ docker/config/db/init.sql
@@ -2130,7 +2130,6 @@ EXECUTE FUNCTION update_update_time();
 
 ------------------------------------------------------------
 
-
 CREATE TABLE "next_console"."llm_instance_info"
 (
  "id" SERIAL PRIMARY KEY,
@@ -2155,15 +2154,20 @@ CREATE TABLE "next_console"."llm_instance_info"
  "max_tokens" integer ,
  "n" integer ,
  "presence_penalty" double precision ,
- "response_format" json,
+ "response_format" json ,
  "stop" json ,
  "stream" boolean ,
  "temperature" double precision ,
  "top_p" double precision ,
  "llm_icon" text ,
- "is_std_openai" boolean  ,
+ "is_std_openai" boolean ,
  "support_vis" boolean ,
- "support_file" boolean
+ "support_file" boolean ,
+ "llm_label" varchar(255) ,
+ "extra_body" json ,
+ "extra_headers" json ,
+ "use_default" boolean ,
+CONSTRAINT "pk_next_console_llm_instance_info" PRIMARY KEY ("id")
 )
 WITH (
     FILLFACTOR = 100,
@@ -2200,6 +2204,10 @@ COMMENT ON COLUMN "next_console"."llm_instance_info"."llm_icon" IS '模型图标
 COMMENT ON COLUMN "next_console"."llm_instance_info"."is_std_openai" IS '是否支持openai-sdk';
 COMMENT ON COLUMN "next_console"."llm_instance_info"."support_vis" IS '是否支持视觉';
 COMMENT ON COLUMN "next_console"."llm_instance_info"."support_file" IS '是否支持文件';
+COMMENT ON COLUMN "next_console"."llm_instance_info"."llm_label" IS '模型显示名称';
+COMMENT ON COLUMN "next_console"."llm_instance_info"."extra_body" IS '额外请求头';
+COMMENT ON COLUMN "next_console"."llm_instance_info"."extra_headers" IS '额外请求体';
+COMMENT ON COLUMN "next_console"."llm_instance_info"."use_default" IS '使用默认参数';
 COMMENT ON TABLE "next_console"."llm_instance_info" IS '基模型实例信息表';
 
 CREATE INDEX "user_id12"
@@ -2208,6 +2216,86 @@ ON "next_console"."llm_instance_info" USING btree ( "user_id" )
 
 CREATE TRIGGER update_llm_instance_info_trigger BEFORE UPDATE ON "next_console"."llm_instance_info" FOR EACH ROW
 EXECUTE FUNCTION update_update_time();
+
+------------------------------------------------------------
+    CREATE TABLE "next_console"."llm_supplier_info"
+(
+  id SERIAL PRIMARY KEY,
+ "supplier_code" varchar(256) ,
+ "supplier_name" varchar(256) ,
+ "supplier_desc" text ,
+ "supplier_icon" text ,
+ "supplier_type" varchar(10) ,
+ "supplier_website" text ,
+ "supplier_models" json ,
+ "supplier_status" varchar(10) ,
+ "create_time" timestamp with time zone DEFAULT CURRENT_TIMESTAMP ,
+ "update_time" timestamp with time zone DEFAULT CURRENT_TIMESTAMP ,
+ "supplier_api_url" text ,
+CONSTRAINT "pk_next_console_llm_supplier_info" PRIMARY KEY ("id")
+)
+WITH (
+    FILLFACTOR = 100,
+    OIDS = FALSE
+)
+;
+COMMENT ON COLUMN "next_console"."llm_supplier_info"."id" IS '自增id';
+COMMENT ON COLUMN "next_console"."llm_supplier_info"."supplier_code" IS '基模型厂商编号';
+COMMENT ON COLUMN "next_console"."llm_supplier_info"."supplier_name" IS '基模型厂商名称';
+COMMENT ON COLUMN "next_console"."llm_supplier_info"."supplier_desc" IS '基模型厂商描述';
+COMMENT ON COLUMN "next_console"."llm_supplier_info"."supplier_icon" IS '基模型厂商图标';
+COMMENT ON COLUMN "next_console"."llm_supplier_info"."supplier_type" IS '基模型厂商类型';
+COMMENT ON COLUMN "next_console"."llm_supplier_info"."supplier_website" IS '基模型厂商官网';
+COMMENT ON COLUMN "next_console"."llm_supplier_info"."supplier_models" IS '基模型厂商支持的模型列表';
+COMMENT ON COLUMN "next_console"."llm_supplier_info"."supplier_status" IS '基模型厂商状态';
+COMMENT ON COLUMN "next_console"."llm_supplier_info"."create_time" IS '创建时间';
+COMMENT ON COLUMN "next_console"."llm_supplier_info"."update_time" IS '更新时间';
+COMMENT ON COLUMN "next_console"."llm_supplier_info"."supplier_api_url" IS '厂商api地址';
+COMMENT ON TABLE "next_console"."llm_supplier_info" IS '基模型厂商信息表';
+
+-----------------------------------------------------------
+
+
+CREATE TABLE "next_console"."llm_instance_authorize_info"
+(
+  id SERIAL PRIMARY KEY,
+ "user_id" integer NOT NULL ,
+ "model_id" integer NOT NULL ,
+ "auth_colleague_id" integer ,
+ "auth_friend_id" integer ,
+ "auth_department_id" integer ,
+ "auth_company_id" integer ,
+ "auth_type" varchar(255) NOT NULL ,
+ "auth_status" varchar(255) NOT NULL ,
+ "create_time" timestamp with time zone DEFAULT CURRENT_TIMESTAMP ,
+ "update_time" timestamp with time zone DEFAULT CURRENT_TIMESTAMP ,
+CONSTRAINT "pk_next_console_llm_instance_authorize_info" PRIMARY KEY ("id")
+)
+WITH (
+    FILLFACTOR = 100,
+    OIDS = FALSE
+)
+;
+COMMENT ON COLUMN "next_console"."llm_instance_authorize_info"."id" IS '自增id';
+COMMENT ON COLUMN "next_console"."llm_instance_authorize_info"."user_id" IS '用户id';
+COMMENT ON COLUMN "next_console"."llm_instance_authorize_info"."model_id" IS '模型id';
+COMMENT ON COLUMN "next_console"."llm_instance_authorize_info"."auth_colleague_id" IS '被授权同事id';
+COMMENT ON COLUMN "next_console"."llm_instance_authorize_info"."auth_friend_id" IS '被授权联系人id';
+COMMENT ON COLUMN "next_console"."llm_instance_authorize_info"."auth_department_id" IS '被授权部门id';
+COMMENT ON COLUMN "next_console"."llm_instance_authorize_info"."auth_company_id" IS '被授权公司id';
+COMMENT ON COLUMN "next_console"."llm_instance_authorize_info"."auth_type" IS '授权类型';
+COMMENT ON COLUMN "next_console"."llm_instance_authorize_info"."auth_status" IS '授权状态';
+COMMENT ON COLUMN "next_console"."llm_instance_authorize_info"."create_time" IS '创建时间';
+COMMENT ON COLUMN "next_console"."llm_instance_authorize_info"."update_time" IS '更新时间';
+COMMENT ON TABLE "next_console"."llm_instance_authorize_info" IS '模型授权用户表';
+
+CREATE INDEX "llm_instance_authorize_user_id49"
+ON "next_console"."llm_instance_authorize_info" USING btree ( "user_id" )
+;
+CREATE INDEX "model_id50"
+ON "next_console"."llm_instance_authorize_info" USING btree ( "model_id" )
+;
+
 
 ------------------------------------------------------------
 CREATE TABLE "next_console"."system_config_info"
@@ -3086,7 +3174,7 @@ INSERT INTO "next_console"."assistant_info" ("id","assistant_name","assistant_de
 
   $$包裹块级公式, 如
 
- $$ \n   \\pi = 4 \\left(1 - \\frac{1}{3} + \\frac{1}{5} - \\frac{1}{7} + \\frac{1}{9} - \\frac{1}{11} + \\cdots \\right) \n   $$。
+ $$ \n  \pi = 4 \left(1 - \frac{1}{3} + \frac{1}{5} - \frac{1}{7} + \frac{1}{9} - \frac{1}{11} + \cdots \right) \n   $$。
 
 
   当用户提问涉及数据的可视化方面问题时，优先使用最新的Mermaid 语法直接渲染数据。
@@ -3946,4 +4034,8 @@ INSERT INTO support_area_info (country,iso_code_2,iso_code_3,phone_code,continen
 INSERT INTO support_area_info (country,iso_code_2,iso_code_3,phone_code,continent,province,city,area_status) VALUES ('中国','CN','CHN','86','亚洲','青海省','果洛藏族自治州','正常');
 INSERT INTO support_area_info (country,iso_code_2,iso_code_3,phone_code,continent,province,city,area_status) VALUES ('中国','CN','CHN','86','亚洲','青海省','玉树藏族自治州','正常');
 INSERT INTO support_area_info (country,iso_code_2,iso_code_3,phone_code,continent,province,city,area_status) VALUES ('中国','CN','CHN','86','亚洲','青海省','海西蒙古族藏族自治州','正常');
+
+-------------------------------
+
+
 
